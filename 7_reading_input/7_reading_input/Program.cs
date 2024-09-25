3379a09e5be9ffcd216b84c21680865e8340b474
Aergu <josaro8260@gmail.com>
2024-09-25 11:22:06 +0200

    7_reading_input

---
+++ 7_reading_input/7_reading_input/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("What's your name?");
+
+string input = Console.ReadLine();
+
+Console.WriteLine("Hello, " + input + "!");
