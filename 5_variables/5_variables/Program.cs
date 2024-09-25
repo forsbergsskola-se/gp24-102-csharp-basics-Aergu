89634f8c8c675e65fd319c1c6a97f583664fe8b3
Aergu <josaro8260@gmail.com>
2024-09-25 10:39:26 +0200

    5_variables

---
+++ 5_variables/5_variables/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+ string a = "Hello";
+ 
+ Console.WriteLine(a);
+ 
+a = "world";
+ Console.WriteLine(a);
+ 
+ string b = a;
+ 
+ Console.WriteLine(b);
+ 
+ a = "Forsbergs";
+ 
+ Console.WriteLine(a);
+ 
+ Console.WriteLine(b);
