9942fbfc03335a2f07c11cf8f5701a8c9731cdec
Aergu <josaro8260@gmail.com>
2024-09-25 15:11:02 +0200

    9_1_speedconverter

---
+++ 9.1_speedconverter/9.1_speedconverter/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Please enter a number. It will be converted from km/h to m/s shortly.");
+
+string input = Console.ReadLine();
+
+int Kmperhour = int.Parse(input);
+
+int ConvertedKmh = (int)(Kmperhour / 3.6f);
+
+Console.WriteLine("m/s is " + ConvertedKmh);
