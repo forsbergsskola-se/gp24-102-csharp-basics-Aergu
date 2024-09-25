542d77fa3620ef4167e9bf88df82fc896bbf41e9
Aergu <josaro8260@gmail.com>
2024-09-25 09:28:23 +0200

    c#basics_assignments

---
+++ lecture2/lecture2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Http.Headers;
+
+Console.WriteLine("Please input the desired size of your triangle:");
+
+retry:
+
+bool parsed = int.TryParse(Console.ReadLine(), out int triangle);
+
+if (!parsed)
+{
+    Console.WriteLine("Please try only with numerical values.");
+    
+}
