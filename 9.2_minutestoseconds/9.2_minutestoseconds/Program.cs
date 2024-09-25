0d2e9ca97d434dcc032069b681e8ba4e696d08a7
Aergu <josaro8260@gmail.com>
2024-09-25 15:29:18 +0200

    9.2_MinutesToSeconds

---
+++ 9.2_minutestoseconds/9.2_minutestoseconds/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Please enter a number. It will be converted from minutes to seconds shortly.");
+
+string input = Console.ReadLine();
+
+int minutes = int.Parse(input);
+
+int ConvertedInteger = minutes * 60;
+
+Console.WriteLine(input + " minutes is " + ConvertedInteger + " seconds.");
