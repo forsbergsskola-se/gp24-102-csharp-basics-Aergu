2ca2cf6061183d579509b7efb3e9539d4504e39c
Aergu <josaro8260@gmail.com>
2024-09-26 10:29:26 +0200

    9.6_negation

---
+++ 9_6_negation/9_6_negation/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Please, enter a number. It's negation will be shown shortly.");
+
+int input = int.Parse(Console.ReadLine());
+
+input -= input + input;
+
+
+Console.WriteLine(input);
+
