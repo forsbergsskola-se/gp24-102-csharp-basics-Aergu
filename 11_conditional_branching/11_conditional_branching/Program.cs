15f6a82fe2c38d9b994af5e8dadf981569840cfb
Aergu <josaro8260@gmail.com>
2024-09-26 15:18:29 +0200

    11.0_conditional_branching

---
+++ 11_conditional_branching/11_conditional_branching/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("What's your age?");
+
+int input = int.Parse( Console.ReadLine());
+
+bool isAnAdult = input > 19;
+Console.WriteLine("You're an adult.");
+
+Console.WriteLine("Give me another integer.");
+
+int input2 = int.Parse(Console.ReadLine());
+
+bool FirstNumberBiggerThanSecondNumber = input > input2;
+if (FirstNumberBiggerThanSecondNumber)
+{
+    Console.WriteLine("The maximum is: " + input);
+}
+
+else
+{
+    Console.WriteLine("The maximum is: " + input2);
+}
+
+if (input % 2 == 0)
+{
+    Console.WriteLine("That number is an even number.");
+}
+
+else
+
+    Console.WriteLine("That number is an odd number.");
+
