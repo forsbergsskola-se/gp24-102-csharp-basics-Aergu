4a324cbcfa1e6f143989cad4581c9a74c3e981c6
Aergu <josaro8260@gmail.com>
2024-09-26 11:53:49 +0200

    9.10_seconds_to_minutes

---
+++ 9.10_seconds_to_minutes/9.10_seconds_to_minutes/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Enter a number. It will be converted from seconds to minutes shortly.");
+
+int input = int.Parse(Console.ReadLine());
+
+int ConvertedInterger = input / 60;
+
+int RemaniningSeconds = input % 60;
+
+Console.WriteLine(ConvertedInterger + " minute(s) and " + RemaniningSeconds + " second(s).");
